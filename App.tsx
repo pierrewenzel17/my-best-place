4d23f071746d230dd303686a30b5ac74edab589c
Pierre Wenzel <pierre.wenzel@outlook.com>
2022-03-27 20:10:46 +0200

    changement de credential

--- App.tsx
+++ App.tsx
@@ -13,12 +13,12 @@ import DetailsScreen from './src/screens/DetailsScreen';
 const Stack = createNativeStackNavigator();
 
 const firebaseConfig = {
-	apiKey: "AIzaSyDws0GdA-TS1UaH3SV-t9ZElSP08YP7Rdg",
-	authDomain: "my-best-place-2.firebaseapp.com",
-	projectId: "my-best-place-2",
-	storageBucket: "my-best-place-2.appspot.com",
-	messagingSenderId: "719862810779",
-	appId: "1:719862810779:web:20220bac85fe3effb79e3e"
+	apiKey: 'AIzaSyCDUS8unENrebvXXAJzCi0-p2ZQlL6AxnY',
+	authDomain: 'my-best-place-3.firebaseapp.com',
+	projectId: 'my-best-place-3',
+	storageBucket: 'my-best-place-3.appspot.com',
+	messagingSenderId: '756566792578',
+	appId: '1:756566792578:web:ede72021e45a30e8a8d79b',
 };
 
 export default function App(): JSX.Element {
@@ -36,15 +36,8 @@ export default function App(): JSX.Element {
 							name='Login'
 							component={ConnectionScreen}
 						/>
-						<Stack.Screen
-							name='Tabs'
-							component={Tabs}
-							options={{ headerShown: false }}
-						/>
-						<Stack.Screen
-							name='Details'
-							component={DetailsScreen}
-						/>
+						<Stack.Screen name='Tabs' component={Tabs} options={{ headerShown: false }} />
+						<Stack.Screen name='Details' component={DetailsScreen} />
 					</Stack.Navigator>
 				</NavigationContainer>
 			</ApplicationProvider>
